363bb09957456a32db7e6061a918a41ea2f0aff4
nick87 <kolyakol8787@gmail.com>
2023-11-19 13:07:49 +0200

    added publish true

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -3,6 +3,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"}, features = "src/test/resources/features")
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = "src/test/resources/features",
+        publish = true)
 public class RunCucumberTest {
 }
